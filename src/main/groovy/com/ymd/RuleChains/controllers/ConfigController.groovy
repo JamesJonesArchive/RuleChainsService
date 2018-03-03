f2ccee0fcb6f7f00a166fa16448a5002e98b0013
James Jones <jamjon3@gmail.com>
2018-03-03 18:53:02 -0500

    Stubbing some controllers for the migration

---
+++ src/main/groovy/com/ymd/RuleChains/controllers/ConfigController.groovy
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ymd.RuleChains.controllers
+
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.multipart.MultipartFile
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.FileSystemResource
+import org.springframework.core.io.ByteArrayResource
+import org.springframework.http.MediaType
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+/**
+ *
+ * @author james
+ */
+@RestController
+class ConfigController {
+	
+}
+
