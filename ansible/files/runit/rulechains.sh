e165eb7cd39626b9174e386a0e1e2f02f00bf197
James Jones <jamjon3@gmail.com>
2018-04-16 18:26:17 -0400

    Moving this to a single multi command

--- ansible/files/runit/rulechains.sh
+++ ansible/files/runit/rulechains.sh
@@ -2,5 +2,5 @@
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 # exec /sbin/setuser memcache /usr/bin/memcached >>/var/log/memcached.log 2>&1
-. /etc/sysconfig/rulechains
-exec /usr/bin/java -Dgrails.env=prod -jar /usr/local/lib/rulechains/RuleChains.jar
+# . /etc/sysconfig/rulechains
+exec source /etc/sysconfig/rulechains; /usr/bin/java -Dgrails.env=prod -jar /usr/local/lib/rulechains/RuleChains.jar
